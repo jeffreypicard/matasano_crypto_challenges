0988007d52b761465f8431e3344bd4e4434ee0be
Jeffrey Picard <jeff@jeffreypicard.com>
2014-09-09 22:26:14 -0400

    Switch everything to uint8_t and have base64_encode only operate on bits, using a separate function to convert to a string. This also removes the space padding.

--- set_1/challenge_1/base64.c
+++ set_1/challenge_1/base64.c
@@ -18,14 +18,28 @@ static const char table[] = {
 	'8', '9', '+', '/'
 };
 
+char *
+bytes_to_string(uint8_t *data, uint32_t len)
+{
+	uint32_t i;
+	char *s = calloc(len + 1, sizeof(char));
+	if (!s) return 0;
+
+	for (i = 0; i < len; i++) {
+		s[i] = table[data[i]];
+	}
+
+	return s;
+}
+
 /*
  * base64_encode
  *
- * encode data (as raw bytes) into base64.
- *
+ * encode data (as raw bytes) into base64 (again raw bytes, *not* string).
+ * 
  */
-char *
-base64_encode(char *data, uint32_t in_len, uint32_t *out_len)
+uint8_t *
+base64_encode(uint8_t *data, uint32_t in_len, uint32_t *out_len)
 {
 	uint32_t i;
 	uint32_t j;
@@ -33,7 +47,7 @@ base64_encode(char *data, uint32_t in_len, uint32_t *out_len)
 	/* if in_len % 3 != 0, we need another 4 bytes */
 	*out_len = 4 * ((in_len + 2) / 3);
 
-	char *out_buf = calloc(*out_len + 1, sizeof(char));
+	uint8_t *out_buf = calloc(*out_len + 1, sizeof(char));
 	if (!out_buf) return 0;
 
 	j = 0;
@@ -42,25 +56,27 @@ base64_encode(char *data, uint32_t in_len, uint32_t *out_len)
 
 		/* Check to see if we're near the end and need one or two equal signs
 		 * FIXME: This could probably be done once before the for loop */
-		unsigned char one_eq = j + 1 < in_len ? 0 : 1;
-		unsigned char two_eq = j + 2 < in_len ? 0 : 1;
+		uint8_t one_eq = j + 1 < in_len ? 0 : 1;
+		uint8_t two_eq = j + 2 < in_len ? 0 : 1;
 
-		unsigned char octet_0 = data[j++];
-		unsigned char octet_1 = one_eq ? 0x00 : data[j++];
-		unsigned char octet_2 = two_eq ? 0x00 : data[j++];
+		uint8_t octet_0 = data[j++];
+		uint8_t octet_1 = one_eq ? 0x00 : data[j++];
+		uint8_t octet_2 = two_eq ? 0x00 : data[j++];
 		buf = octet_0 << 16 | octet_1 << 8 | octet_2;
 
-		unsigned char sextet_0 = buf >> 18;
-		unsigned char sextet_1 = (buf >> 12) & 0x3F;
-		unsigned char sextet_2 = (buf >> 6) & 0x3F;
-		unsigned char sextet_3 = buf & 0x3F;
+		uint8_t sextet_0 = buf >> 18;
+		uint8_t sextet_1 = (buf >> 12) & 0x3F;
+		uint8_t sextet_2 = (buf >> 6) & 0x3F;
+		uint8_t sextet_3 = buf & 0x3F;
 		
-		out_buf[i] = table[sextet_0];
-		out_buf[i+1] = table[sextet_1];
-		out_buf[i+2] = one_eq ? '=' : table[sextet_2];
-		out_buf[i+3] = two_eq ? '=' : table[sextet_3];
+		out_buf[i] = sextet_0;
+		out_buf[i+1] = sextet_1;
+		out_buf[i+2] = one_eq ? 0xff : sextet_2;
+		out_buf[i+3] = two_eq ? 0xff : sextet_3;
+
+		if (one_eq) *out_len = *out_len - 1;
+		if (two_eq) *out_len = *out_len - 1;
 	}
-	out_buf[*out_len] = 0;
 
 	return out_buf;
 }
--- set_1/challenge_1/main.c
+++ set_1/challenge_1/main.c
@@ -8,39 +8,41 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-char *base64_encode(char *data, uint32_t in_len, uint32_t *out_len);
+
+uint8_t *base64_encode(uint8_t *data, uint32_t in_len, uint32_t *out_len);
+char *bytes_to_string(uint8_t *data, uint32_t len);
 
 int
 main(int argc, char *argv[])
 {
 	int x = 0xffffffff;
-	char *a = "Man";
-	char *b = "Ma";
-	char *c = "M";
-	char *words =
+	uint8_t *a = (uint8_t *)"Man";
+	uint8_t *b = (uint8_t *)"Ma";
+	uint8_t *c = (uint8_t *)"M";
+	uint8_t *words = (uint8_t *)
 		"Man is distinguished, not only by his reason, but by this singular passion from "
 		"other animals, which is a lust of the mind, that by a perseverance of delight "
 		"in the continued and indefatigable generation of knowledge, exceeds the short "
 		"vehemence of any carnal pleasure.";
-	char *matasano =
+	uint8_t *matasano = (uint8_t *)
 		"\x49\x27\x6d\x20\x6b\x69\x6c\x6c\x69\x6e\x67\x20\x79\x6f\x75\x72"
 		"\x20\x62\x72\x61\x69\x6e\x20\x6c\x69\x6b\x65\x20\x61\x20\x70\x6f"
 		"\x69\x73\x6f\x6e\x6f\x75\x73\x20\x6d\x75\x73\x68\x72\x6f\x6f\x6d";
 	uint32_t len;
-	char *s;
+	uint8_t *s;
 
-	s = base64_encode((char *)&x, 4, &len);
-	printf("%s\n", s);
+	s = base64_encode((uint8_t *)&x, 4, &len);
+	printf("%s\n", bytes_to_string(s, len));
 	s = base64_encode(a, 3, &len);
-	printf("%s\n", s);
+	printf("%s\n", bytes_to_string(s, len));
 	s = base64_encode(b, 2, &len);
-	printf("%s\n", s);
+	printf("%s\n", bytes_to_string(s, len));
 	s = base64_encode(c, 1, &len);
-	printf("%s\n", s);
-	s = base64_encode(words, strlen(words), &len);
-	printf("%s\n", s);
-	s = base64_encode(matasano, strlen(matasano), &len);
-	printf("%s\n", s);
+	printf("%s\n", bytes_to_string(s, len));
+	s = base64_encode(words, strlen((char*)words), &len);
+	printf("%s\n", bytes_to_string(s, len));
+	s = base64_encode(matasano, strlen((char*)matasano), &len);
+	printf("%s\n", bytes_to_string(s, len));
 
 	return 0;
 }
